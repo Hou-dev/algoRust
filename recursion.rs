c4a340eef12a119a1966e10ad884737c4a08422d
Javed Sahadat <javedsahadatone@gmail.com>
2022-07-07 13:37:25 -0500

    simple recursion example using factorials

--- recursion.rs
+++ recursion.rs
@@ -1,13 +1,13 @@
-fn recursiveCall(x: i16) -> i16{
-    if x == 1{
-        return x;
+fn recursiveCall(x: &mut i16) -> i16{
+    if *x == 1{
+        return *x;
     }
     else{
-        return x * recursiveCall(x-1)
+        return *x * recursiveCall(&mut (*&mut *x-1));
     }
 }
 
 fn main(){
     let mut x = 5;
-    println!("{}",recursiveCall(x))
+    println!("{}",recursiveCall(&mut x));
 }
